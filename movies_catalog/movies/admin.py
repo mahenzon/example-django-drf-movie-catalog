77e0b8d2221640e95675e71717140854bf58a97a
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-12 18:42:14 +0300

    create admin for Movie model

--- movies_catalog/movies/admin.py
+++ movies_catalog/movies/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from movies.models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "description",
+        "release_date",
+        "duration",
+    )
+    list_display_links = (
+        "id",
+        "title",
+    )
+    list_filter = (
+        "title",
+        "release_date",
+    )
+    search_fields = (
+        "title",
+        "description",
+    )
