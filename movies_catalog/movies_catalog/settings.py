4432fa3c94e998319e2533f7b24cb68ca8723cd1
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-12 18:09:35 +0300

    init django app movies

--- movies_catalog/movies_catalog/settings.py
+++ movies_catalog/movies_catalog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # site packages
+    # my
+    "movies.apps.MoviesConfig",
 ]
 
 MIDDLEWARE = [
