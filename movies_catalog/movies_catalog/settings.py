acff25162ae13ff79004f4a99aaa0a81266b32ce
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-12 19:05:14 +0300

    set pagination for REST framework

--- movies_catalog/movies_catalog/settings.py
+++ movies_catalog/movies_catalog/settings.py
@@ -129,3 +129,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# REST Framework
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+}
