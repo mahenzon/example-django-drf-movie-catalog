5f92824685ec2223473b7c0fbf4c96e5a4d4878c
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-12 18:18:04 +0300

    configure project for pg

--- movies_catalog/movies_catalog/settings.py
+++ movies_catalog/movies_catalog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from os import getenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,9 +78,13 @@ WSGI_APPLICATION = "movies_catalog.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": getenv("DB_NAME", "movies_catalog"),
+        "USER": getenv("DB_USER", "app"),
+        "PASSWORD": getenv("DB_PASSWORD", "supersecretpassword"),
+        "HOST": getenv("DB_HOST", "localhost"),
+        "PORT": getenv("DB_PORT", 5432),
+    },
 }
 
 
