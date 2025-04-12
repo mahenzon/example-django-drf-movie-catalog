71f1f8ef95f7a85afd4cb99bc6660762d9c92ad6
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-12 19:04:47 +0300

    create urls and router for movies api

---
+++ movies_catalog/movies/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from movies import views
+
+router = DefaultRouter()
+router.register("movies", views.MovieViewSet)
+
+app_name = "movies"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
