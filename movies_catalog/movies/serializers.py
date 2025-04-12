94a990c0543576fc870c4a1993c8ce8f035d0485
Suren Khorenyan <surenkhorenyan@gmail.com>
2025-04-12 19:04:18 +0300

    create MovieSerializer

---
+++ movies_catalog/movies/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from movies.models import Movie
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = (
+            "id",
+            "title",
+            "description",
+            "release_date",
+            "duration",
+        )
