6963fa86593c0c449db4fc05ed331fd7d8143201
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-01-31 19:52:25 +0300

    configure drf_spectacular views

--- movies_catalog/movies_catalog/urls.py
+++ movies_catalog/movies_catalog/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
     path("api/", include("movies.urls")),
@@ -27,5 +32,23 @@ urlpatterns = [
             namespace="rest_framework",
         ),
     ),
+    # DRF-spectacular
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
+    ),
+    # Optional UI:
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    # // DRF-spectacular
     path("admin/", admin.site.urls),
 ]
