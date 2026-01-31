577a52f03846e67feda81e79fb5953695c38013e
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-01-31 20:24:23 +0300

    add description for retrieve

--- movies_catalog/movies/views.py
+++ movies_catalog/movies/views.py
@@ -1,8 +1,10 @@
 from textwrap import dedent
 
 from django.db.models import QuerySet
+from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import (
     OpenApiExample,
+    OpenApiParameter,
     OpenApiResponse,
     extend_schema,
     extend_schema_view,
@@ -16,6 +18,14 @@ from movies.serializers import (
     AgeRatingSerializer,
     MovieDetailSerializerExtended,
     MovieSerializer,
+)
+
+INCLUDE_MOVIE_RELATIONS_QUERY_PARAM = OpenApiParameter(
+    "include",
+    OpenApiTypes.STR,
+    # OpenApiParameter.QUERY,
+    description="include age_rating and genre",
+    default="1",
 )
 
 
@@ -42,6 +52,9 @@ from movies.serializers import (
             ```
             """,
         ),
+        parameters=[
+            INCLUDE_MOVIE_RELATIONS_QUERY_PARAM,
+        ],
     ),
     create=extend_schema(
         responses={
@@ -64,6 +77,72 @@ from movies.serializers import (
                                 "This field is required.",
                             ],
                         },
+                    ),
+                ],
+            ),
+        },
+    ),
+    retrieve=extend_schema(
+        description=dedent(
+            """
+            ## Retrieve Movie details by id
+
+            Returns movie details. Use the `include` query parameter to
+            include nested `age_rating` and `genres` relationships.
+
+            **Example API call (without include):**
+
+            ```bash
+            curl -X 'GET' 'http://127.0.0.1:8000/api/movies/1/'
+            ```
+
+            **Example API call (with include):**
+
+            ```bash
+            curl -X 'GET' 'http://127.0.0.1:8000/api/movies/1/?include=1'
+            ```
+            """,
+        ),
+        parameters=[
+            INCLUDE_MOVIE_RELATIONS_QUERY_PARAM,
+        ],
+        responses={
+            status.HTTP_200_OK: OpenApiResponse(
+                # response=MovieDetailSerializerExtended,
+                response="application/json",
+                description="Movie details (without nested relations)",
+                examples=[
+                    OpenApiExample(
+                        name="Response without include",
+                        value={
+                            "id": 1,
+                            "title": "Movie Name",
+                            "description": "Movie description",
+                            "release_date": "2025-09-26",
+                            "duration": 95,
+                            "age_rating": "R",
+                        },
+                        response_only=True,
+                    ),
+                    OpenApiExample(
+                        name="Response with include=1",
+                        value={
+                            "id": 1,
+                            "title": "Movie Name",
+                            "description": "Movie description",
+                            "release_date": "2025-09-26",
+                            "duration": 95,
+                            "age_rating": {
+                                "id": 1,
+                                "symbol": "R",
+                                "description": "Restricted",
+                            },
+                            "genres": [
+                                {"id": 1, "name": "Action"},
+                                {"id": 2, "name": "Drama"},
+                            ],
+                        },
+                        response_only=True,
                     ),
                 ],
             ),
