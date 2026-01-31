cc3902e3bb0f766ea1e0f37cab259e663ae08280
Suren Khorenyan <surenkhorenyan@gmail.com>
2026-01-31 19:53:47 +0300

    add schema for views

--- movies_catalog/movies/views.py
+++ movies_catalog/movies/views.py
@@ -1,5 +1,13 @@
+from textwrap import dedent
+
 from django.db.models import QuerySet
-from rest_framework import viewsets
+from drf_spectacular.utils import (
+    OpenApiExample,
+    OpenApiResponse,
+    extend_schema,
+    extend_schema_view,
+)
+from rest_framework import status, viewsets
 from rest_framework.serializers import Serializer
 
 from movies.models import AgeRating, Movie
@@ -11,13 +19,66 @@ from movies.serializers import (
 )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        description=dedent(
+            """
+            ## List available movies
+
+            Example list:
+
+            - one
+            - two
+            - three with inner:
+                - a
+                - b
+                - c
+            - four
+
+            **Example API call:**
+
+            ```bash
+            curl -X 'GET' 'http://127.0.0.1:8000/api/movies/'
+            ```
+            """,
+        ),
+    ),
+    create=extend_schema(
+        responses={
+            status.HTTP_201_CREATED: MovieSerializer,
+            status.HTTP_400_BAD_REQUEST: OpenApiResponse(
+                response="application/json",
+                examples=[
+                    OpenApiExample(
+                        name="Invalid FK example",
+                        value={
+                            "age_rating": [
+                                'Invalid pk "foobar" - object does not exist.',
+                            ],
+                        },
+                    ),
+                    OpenApiExample(
+                        name="Missing required fields",
+                        value={
+                            "title": [
+                                "This field is required.",
+                            ],
+                        },
+                    ),
+                ],
+            ),
+        },
+    ),
+)
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
 
     def get_queryset(self) -> QuerySet:
         qs = self.queryset
-        if self.action == "retrieve":
+        # if self.action == "retrieve":
+        if self.request.method == "GET" and self.request.GET.get("include"):
             qs = qs.select_related("age_rating")
+            qs = qs.prefetch_related("genres")
         return qs
 
     def get_serializer_class(self) -> type[Serializer]:
